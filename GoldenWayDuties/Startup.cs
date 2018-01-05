2f2735f0cc603932491305412deb3ad09512932d
Tapi200 <takwada2000@yahoo.com>
2018-01-06 01:18:45 +1100

    Add project files.

---
+++ GoldenWayDuties/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GoldenWayDuties.Startup))]
+namespace GoldenWayDuties
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
